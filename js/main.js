87ef38c80bcce0c20d1f5233f134b52b26614756
Tusar <asktusar@gmail.com>
2013-11-20 17:20:46 +0530

    adding new files

--- js/main.js
+++
@@ -1,6 +0,0 @@
-function onLoad() {
-	document.addEventListener("deviceready", onDeviceReady, false);
-}
-function onDeviceReady() {
-	window.plugins.childBrowser.showWebPage('http://staging.graphemetech.co.in/gita/', { showLocationBar: true });
-}
