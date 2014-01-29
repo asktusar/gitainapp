0bdf754a1797e88d29b9b4482eca6fc91a2203a7
sandeep <sandeep@gmail.com>
2014-01-29 12:30:32 +0530

    changed js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ function onLoad() {
 	document.addEventListener("deviceready", onDeviceReady, false);
 }
 function onDeviceReady() {
-	window.plugins.childBrowser.showWebPage('http://staging.graphemetech.co.in/gita/', { showLocationBar: true });
+	window.plugins.childBrowser.showWebPage('http://google.co.in', { showLocationBar: true });
 }
